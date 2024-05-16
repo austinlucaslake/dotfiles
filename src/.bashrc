8d84c11bbce8067b9fa9d748a2ee949b283955d0
Austin Lucas Lake <git@austinlucaslake.com>
2024-05-16 15:17:06 -0700

    new update script and updated dotfiles

--- src/.bashrc
+++ src/.bashrc
@@ -1,2 +1,3 @@
+eval "$(ssh-agent -s)"
 source ~/.cargo/env
 eval "$(starship init bash)"
--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-export SCRIPTPATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SCRIPTPATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-cp -r ~/.config/nvim ${SCRIPTPATH}/src/.config
-cp -r ~/.config/tmux ${SCRIPTPATH}/src/.config
-cp -r ~/.config/alacritty ${SCRIPTPATH}/src/.config
-
-cp ~/.bash_profile ${SCRIPTPATH}/src
-cp ~/.bashrc ${SCRIPTPATH}/src
-cp ~/.hushlogin ${SCRIPTPATH}/src
+cp $HOME/.bash_profile $SCRIPTPATH/src/
+cp $HOME/.bashrc $SCRIPTPATH/src/
+cp $HOME/.gitconfig $SCRIPTPATH/src/
+sed -i --follow-symlinks -e "s/signingkey.*[0-9A-Z]*/signingkey = {{ gpg_key.fingerprints[0] }}/g" $SCRIPTPATH/src/.gitconfig
+cp $HOME/.hushlogin $SCRIPTPATH/src/
+cp -r $HOME/.config/alacritty $SCRIPTPATH/src/.config/
+cp -r $HOME/.config/nvim $SCRIPTPATH/src/.config/
+cp -r $HOME/.config/tmux $SCRIPTPATH/src/.config/
+cp -r $HOME/.gnupg/gpg-agent.conf $SCRIPTPATH/src/.gnupg/
