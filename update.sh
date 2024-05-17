fdc7cfe967d61855c6d3684c3ca507ca8727da97
Austin Lucas Lake <git@austinlucaslake.com>
2024-05-17 15:13:01 -0700

    added i3 placeholder for ansible testing

--- update.sh
+++ update.sh
@@ -4,10 +4,12 @@ SCRIPTPATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cp $HOME/.bash_profile $SCRIPTPATH/src/
 cp $HOME/.bashrc $SCRIPTPATH/src/
-cp $HOME/.gitconfig $SCRIPTPATH/src/
-sed -i --follow-symlinks -e "s/signingkey.*[0-9A-Z]*/signingkey = {{ gpg_key.fingerprints[0] }}/g" $SCRIPTPATH/src/.gitconfig
-cp $HOME/.hushlogin $SCRIPTPATH/src/
 cp -r $HOME/.config/alacritty $SCRIPTPATH/src/.config/
+cp -r $HOME/.config/i3 $SCRIPTPATH/src/.config/
 cp -r $HOME/.config/nvim $SCRIPTPATH/src/.config/
 cp -r $HOME/.config/tmux $SCRIPTPATH/src/.config/
+cp $HOME/.gitconfig $SCRIPTPATH/src/
+sed -i --follow-symlinks -e "s/signingkey.*[0-9A-Z]*/signingkey = {{ gpg_key.fingerprints[0] }}/g" $SCRIPTPATH/src/.gitconfig
 cp -r $HOME/.gnupg/gpg-agent.conf $SCRIPTPATH/src/.gnupg/
+cp $HOME/.hushlogin $SCRIPTPATH/src/
+
