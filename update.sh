1ec99380625cb2c8a33f7cf41aa62f1ab74e80ee
Austin Lucas Lake <git@austinlucaslake.com>
2024-05-18 23:03:24 -0700

    updated config files and update script

--- update.sh
+++ update.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-SCRIPTPATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SCRIPTPATH=$(cd "$(dirname '${BASH_SOURCE[0]}')" && pwd)
 
-cp $HOME/.bash_profile $SCRIPTPATH/src/
 cp $HOME/.bashrc $SCRIPTPATH/src/
-cp -r $HOME/.config/alacritty $SCRIPTPATH/src/.config/
-cp -r $HOME/.config/i3 $SCRIPTPATH/src/.config/
-cp -r $HOME/.config/nvim $SCRIPTPATH/src/.config/
-cp -r $HOME/.config/tmux $SCRIPTPATH/src/.config/
+cp -r $HOME/.config/alacritty/alacritty.yaml $SCRIPTPATH/src/.config/alacritty/
+cp -r $HOME/.config/i3/config $SCRIPTPATH/src/.config/i3/
+cp -r $HOME/.config/nvim/init.lua $SCRIPTPATH/src/.config/nvim/
+cp -r $HOME/.config/tmux/tmux.conf $SCRIPTPATH/src/.config/tmux/
 cp $HOME/.gitconfig $SCRIPTPATH/src/
 sed -i --follow-symlinks -e "s/signingkey.*[0-9A-Z]*/signingkey = {{ gpg_key.fingerprints[0] }}/g" $SCRIPTPATH/src/.gitconfig
 cp -r $HOME/.gnupg/gpg-agent.conf $SCRIPTPATH/src/.gnupg/
-cp $HOME/.hushlogin $SCRIPTPATH/src/
 
